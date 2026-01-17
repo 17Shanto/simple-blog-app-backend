2452d5c13ee9a6eb57d480e93257493dce660db8
17Shanto <shahporanshanto23@gmail.com>
2026-01-17 23:36:03 +0600

    feat: add admin

--- simpleblog/settings.py
+++ simpleblog/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts',
-    'accounts'
+    'accounts',
     
     #Third-party-apps
     'rest_framework'
 ]
+
+AUTH_USER_MODEL = "accounts.User"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
